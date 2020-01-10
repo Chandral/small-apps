4ace3e224346bad1d8b357804fc79a5a1463f144
Chandral Thakor <chandral.thakor@gmail.com>
2020-01-10 15:23:07 +0530

    Program now adds sites to '/etc/hosts' to block them in working hours.

--- site-blocker/app.py
+++ site-blocker/app.py
@@ -8,6 +8,13 @@ site_list = ["www.twitter.com", "www.facebook.com"]
 while True:
     if (dt.now().hour >= 10) and (dt.now().hour <= 18):
         print("Working")
+        with open(hosts_path, 'r+') as file:
+            content = file.read()
+            for site in site_list:
+                if site in content:
+                    continue
+                else:
+                    file.write(redirect + ' ' + site + '\n')
     else:
         print("Happy Hours")
     time.sleep(5)
